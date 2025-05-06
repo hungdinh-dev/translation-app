619dc16cf8abd33b19b762b0f095d601617543d7
hungdinh-dev <baohungdinhnguyen1412@gmail.com>
2025-05-06 16:58:34 +0700

    bug fixes next auth

---
+++ src/components/auth/auth-options.ts
@@ -0,0 +1,74 @@
+import NextAuth, { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                const { email, password } = credentials as Record<"email" | "password", string>;
+
+                const user = await prisma.authProvider.findUnique({
+                    where: {
+                        provider_providerId: {
+                            provider: "credentials",
+                            providerId: email,
+                        },
+                    },
+                    include: { user: true },
+                });
+
+                if (!user || !user.passwordHash) {
+                    return null;
+                }
+
+                const passwordMatch = await bcrypt.compare(password, user.passwordHash);
+
+                if (passwordMatch) {
+                    return user.user;
+                } else {
+                    return null;
+                }
+            },
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+                token.name = user.name;
+                token.email = user.email;
+                token.image = user.image;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user.id = token.id;
+                session.user.name = token.name;
+                session.user.email = token.email;
+                session.user.image = token.image as string | null | undefined;
+            }
+            return session;
+        },
+    },
+};
